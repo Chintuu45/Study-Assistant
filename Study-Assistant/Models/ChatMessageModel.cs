b52dc39abee7822ad4284136b06326f430a5a280
Panchanan Behera <beherapanchanan933@gmail.com>
2024-08-28 14:15:47 +0530

    Add files via upload

---
+++ Study-Assistant/Models/ChatMessageModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace StudyAssistant.Models
+{
+    public enum AuthorType
+    {
+        User,
+        AI
+    }
+    public class ChatMessageModel(AuthorType authorType, string message, bool lastMessage = false) : INotifyPropertyChanged
+    {
+        public AuthorType Author { get; set; } = authorType;
+        public string Message { get; set; } = message;
+
+        private bool _lastMessage = lastMessage;
+        public bool LastMessage
+        {
+            get => _lastMessage;
+            set
+            {
+                if (_lastMessage != value)
+                {
+                    _lastMessage = value;
+                    OnPropertyChanged(nameof(LastMessage));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
